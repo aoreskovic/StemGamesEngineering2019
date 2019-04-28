c7d4a5c74b6d2f1a91bdd2984cbd20eb1ca3d880
Ante Orešković <ante.oreskovic@outlook.com>
2019-04-28 20:56:19 +0200

    Created functions for sub mass and power

--- Submarine/submarine_mechanical.m
+++ Submarine/submarine_mechanical.m
@@ -17,8 +17,8 @@ faktor_strukture = 1.5
 
 max_depth = 100; % m
 
-sub_l = 18 % m - length
-R = 1.5 % m - radius
+cilynder_l = 18 % m - length
+sub_radious = 1.5 % m - radius
 
 speed_kn = 7
 
@@ -33,30 +33,30 @@ p_h = row * g * max_depth % Pa - Hydrostatic pressure
 p_h_bar = p_h / 100000 % Bar - Hydrostatic pressure
 
 
-L = sub_l + 2 * R
+sub_length = cilynder_l + 2 * sub_radious
 
 
 % Volume
 
-vol_cylinder = R^2 * sub_l * pi; % m^3
-vol_sphere = 4/3 * R^3 * pi; % m^3
+vol_cylinder = sub_radious^2 * cilynder_l * pi; % m^3
+vol_sphere = 4/3 * sub_radious^3 * pi; % m^3
 sub_volume = vol_cylinder + vol_sphere % m^3
 
 istisnina = row * sub_volume % t
 
-CB =  sub_volume / (L * 2 * R * 2 * R) % Block coefficient
+CB =  sub_volume / (sub_length * 2 * sub_radious * 2 * sub_radious) % Block coefficient
 
 
 % Surface
 
-sur_cylinder = 2 * R * pi * sub_l
-sur_sphere =  4 * R^2 * pi
+sur_cylinder = 2 * sub_radious * pi * cilynder_l
+sur_sphere =  4 * sub_radious^2 * pi
 
 sub_surface = sur_cylinder + sur_sphere
 
 % Debljina stjenke
 
-Di = 2 * R * 1000
+Di = 2 * sub_radious * 1000
 
 sub_stijenka = p_h_bar * Di / (20 * sigma * faktor_zavarivanja + p_h_bar)
 
@@ -68,14 +68,14 @@ sub_masa_celika = sub_surface * sub_stijenka/1000 * roc/1000 * faktor_strukture
 speed_ms = speed_kn * 0.514444;
 
 ni = 0.000001187 % m^2/s - Kinematic vicscosity 
-Rn = speed_ms * L / ni % Reynolds_number
+Rn = speed_ms * sub_length / ni % Reynolds_number
 
 CF = 0.075/(log10(Rn)-2)^2 % Friction coefficient
 
-Fn = speed_ms /sqrt(9.08665 * L)
+Fn = speed_ms /sqrt(9.08665 * sub_length)
 
 
-k=-0.095+25.6 *CB/((L/(2*R))^2)
+k=-0.095+25.6 *CB/((sub_length/(2*sub_radious))^2)
 
 CT = CF * (1 + k)
 

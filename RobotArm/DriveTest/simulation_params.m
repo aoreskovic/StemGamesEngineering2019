989d666ce71f005020cd6bf1a54969b30993f570
Marko Svec <marko.svec@fer.hr>
2019-04-29 16:02:26 +0200

    Crane drive test added

---
+++ RobotArm/DriveTest/simulation_params.m
@@ -0,0 +1,35 @@
+%% SIMULATION DATA
+Tsim = 100;
+Ts = 50e-3;
+
+%% HYDRAULIC DATA
+% Hydraulic rot
+rotRadiusOut = 110;
+rotRadiusIn = 100;
+rotRadiusShaft = 25;
+rotStroke = 800;
+rotRadiusDeadVol = 90;
+rotHeightDeadVol = 30;
+
+rotPistonAreaA = rotRadiusIn^2*pi;
+rotPistonAreaB = rotPistonAreaA - rotRadiusShaft^2*pi;
+rotDeadVolA = rotRadiusDeadVol^2*pi*rotHeightDeadVol;
+rotDeadVolB = rotDeadVolA - rotRadiusShaft^2*pi*rotHeightDeadVol;
+
+rotDamping = 1e5;
+
+% Hydraulic trans
+transRadiusOut = 85;
+transRadiusIn = 75;
+transStroke = 3500;
+transRadiusShaft = 37.5/2;
+transRadiusDeadVol = 65;
+transHeightDeadVol = 30;
+
+transPistonAreaA = transRadiusIn^2*pi;
+transPistonAreaB = transPistonAreaA - transRadiusShaft^2*pi;
+transDeadVolA = transRadiusDeadVol^2*pi*transHeightDeadVol;
+transDeadVolB = transDeadVolA - transRadiusShaft^2*pi*transHeightDeadVol;
+
+transOneDamping = 5e4;
+transTwoDamping = 5e4;
---
+++ RobotArm/DriveTest/testDriving.m
@@ -0,0 +1,15 @@
+clear all; clc;
+
+simulation_params;
+Tsim = 300;
+
+pointOne = [8 5.65 0];
+pointTwo = [-7.5 3.4 7.5]; %Rijeseno
+pointThree = [0 2.9 2.5]; %Rijeseno
+
+refPointsVector = [pointThree, pointOne, pointThree, pointTwo, pointThree];
+
+actuatorRefOne = [0, 0.7, 3.4, 3.4, 6150];
+actuatorRefTwo = [-135, 0.415, 3.4, 3.4, 4300];
+actuatorRefThree = [-90, 0.8, 0, 0, 2000];
+actuatorRefVector = [actuatorRefThree; actuatorRefOne,; actuatorRefThree; actuatorRefTwo; actuatorRefThree];

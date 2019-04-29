77ade85abc07350531385f18f041bc7b31461aed
msvec94 <marko.schwetz@gmail.com>
2019-04-29 21:05:28 +0200

    Crane simulation modified to be executed faster, load coordinate system set at the end of the load.

--- RobotArm/kran/simulation_params.m
+++ RobotArm/kran/simulation_params.m
@@ -1,12 +1,15 @@
 %% SIMULATION DATA
-Tsim = 100;
-Ts = 50e-3;
+Tsim = 300;
+Ts = 0.1;
 
 % Driving test
 pointOne = [8 5.65 0]; %Promijeniti
 pointTwo = [-7.5 3.4 7.5]; %Promijeniti
 pointThree = [0 2.9 2.5]; %Promijeniti
 refPointsVector = [pointThree, pointOne, pointThree, pointTwo, pointThree];
+
+distanceGain = 5/2;
+timeGain = 500/3;
 
 %% HYDRAULIC DATA
 % Hydraulic rot
@@ -22,11 +25,11 @@ rotPistonAreaB = rotPistonAreaA - rotRadiusShaft^2*pi;
 rotDeadVolA = rotRadiusDeadVol^2*pi*rotHeightDeadVol;
 rotDeadVolB = rotDeadVolA - rotRadiusShaft^2*pi*rotHeightDeadVol;
 
-rotDamping = 1e5;
+rotDamping = 5e5;
 
 % Hydraulic trans
 transRadiusOut = 85;
-transRadiusIn = 75;
+transRadiusIn = 50;
 transStroke = 3500;
 transRadiusShaft = 37.5/2;
 transRadiusDeadVol = 65;
@@ -37,6 +40,6 @@ transPistonAreaB = transPistonAreaA - transRadiusShaft^2*pi;
 transDeadVolA = transRadiusDeadVol^2*pi*transHeightDeadVol;
 transDeadVolB = transDeadVolA - transRadiusShaft^2*pi*transHeightDeadVol;
 
-transOneDamping = 5e4;
-transTwoDamping = 5e4;
+transOneDamping = 5e5;
+transTwoDamping = 5e5;
 

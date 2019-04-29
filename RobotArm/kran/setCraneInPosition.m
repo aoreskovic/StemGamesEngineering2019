efb9cb2720df4e23e1d0d0aa5df1530d5f584cb4
Marko Svec <marko.svec@fer.hr>
2019-04-29 12:08:29 +0200

    Fixed direct kinematics and modified Simscape models

--- RobotArm/kran/setCraneInPosition.m
+++ RobotArm/kran/setCraneInPosition.m
@@ -59,7 +59,7 @@ function y = setCraneInPosition(table)
               "/BeltAndPulley/Revolute Joint", 'MaskValueString', str);
     
     % Calculate top of the crane position and set load position
-    L = 4.85949 + table.q3 + table.q4 - 0.15;
+    L = 4.85949 + table.q3 + table.q4;
     a = 1.81648;
     b = 0.6634945;
     c0 = 1.331 + 0.194;
@@ -71,12 +71,13 @@ function y = setCraneInPosition(table)
     theta = gamma - gamma0;
 
     x0 = -0.24164;
-    y0 = 2.640 - 0.5;
-    deltaY = R*deg2rad(table.q5);
-    
-    xp = (x0+ L*cos(theta))*cos(deg2rad(table.q1));
-    yp = y0 + L*sin(theta) - deltaY;
-    zp = -(x0+ L*cos(theta))*sin(deg2rad(table.q1));
+    y0 = 2.640;
+    deltaY = (R*deg2rad(table.q5) - table.q3 - table.q4 - 0.31*sin(theta))/2 + 0.31*sin(theta);
+
+    xp = (x0+ (L-0.15)*cos(theta))*cos(deg2rad(table.q1));
+    yp = y0 + L*sin(theta) - deltaY  - 0.5;
+    zp = -(x0+ (L-0.15)*cos(theta))*sin(deg2rad(table.q1));
+
     position_str = "[" + num2str([xp yp zp]) + "]";
     angle_str =  num2str(table.q1 + 180);
     

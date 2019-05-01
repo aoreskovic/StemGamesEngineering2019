427ffdc7aa0ac8009818747e5966f283a27baf83
msvec94 <marko.schwetz@gmail.com>
2019-05-01 22:20:26 +0200

    Function for writing controller test results added

--- RobotArm/Driving/testKinematics.m
+++ RobotArm/Driving/testKinematics.m
@@ -1,9 +1,10 @@
-table.q1 = 0;
-table.q2 = 0.7;
-table.q3 = 3.4;
-table.q4 = 3.4;
-table.q5 = 6150;
+table.q1 = actuatorRefTwo(1);
+table.q2 = actuatorRefTwo(2);
+table.q3 = actuatorRefTwo(3);
+table.q4 = actuatorRefTwo(4);
+table.q5 = actuatorRefTwo(5);
 
+% Calculate top of the crane position and set load position
 L = 4.85949 + table.q3 + table.q4;
 a = 1.81648;
 b = 0.6634945;
@@ -17,11 +18,12 @@ theta = gamma - gamma0;
 
 x0 = -0.24164;
 y0 = 2.640;
-deltaY = (R*deg2rad(table.q5) - table.q3 - table.q4 - 0.31*sin(theta))/2 + 0.31*sin(theta);
+deltaY = (1 + R*deg2rad(table.q5) - table.q3 - table.q4 - 0.36*sin(theta))/2 + 0.36*sin(theta);
 
-xp = (x0+ (L-0.15)*cos(theta))*cos(deg2rad(table.q1));
-yp = y0 + L*sin(theta) - deltaY  - 0.5 -0.825;
-zp = -(x0+ (L-0.15)*cos(theta))*sin(deg2rad(table.q1));
+xp = (x0+ (L-0.18)*cos(theta))*cos(deg2rad(table.q1));
+yp = y0 + L*sin(theta) - deltaY - 1.325;
+zp = -(x0+ (L-0.18)*cos(theta))*sin(deg2rad(table.q1));
+position = [xp, yp, zp];
 
 xt = (x0+ L*cos(theta))*cos(deg2rad(table.q1));
 yt = y0 + L*sin(theta);

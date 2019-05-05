e7359dab1099540fe0371c262687136c84ac9367
Ante Orešković <ante.oreskovic@gideonbros.ai>
2019-05-05 17:42:00 +0200

    Polished the equations and started work on the Submarine class

--- Submarine/sub_mass.m
+++ Submarine/sub_mass.m
@@ -1,7 +1,9 @@
-function [sub_masa_celika] = sub_mass(cilynder_l, sub_radious, max_depth, faktor_zavarivanja, faktor_strukture)
+function [mass_steel] = sub_mass(length_c, radius_r, max_depth, k_weld, faktor_strukture)
 %sub_mass calculates the mass of a given submarine
 %   Detailed explanation goes here
 
+
+%% Constants
 
 % Densities
 row = 1025; % kg/m^3 - Salt water
@@ -11,41 +13,65 @@ rop = 1300; % kg/m^3 - Plastics
 % Physical constants
 g = 9.81; % m/s^2
 
+max_depth = 50;
+passenger_count2 = 2;
+travel_time = 24;
+travel_range = 500;
+battery_capacity = 2;
+speed_kn = 7;
+
+MCR = 0.85;
+
+
 sigma = 125;
+k_weld = 1;
+
+length_c = 11.05;   % m
+radius_r = 1.5;     % m
+radius_rb = 2.2;    % m
+
+power_density_turbine = 0.1; % t/kW
 
 
-p_h = row * g * max_depth; % Pa - Hydrostatic pressure
-p_h_bar = p_h / 100000; % Bar - Hydrostatic pressure
+%% Calculations
 
 
-sub_length = cilynder_l + 2 * sub_radious;
-
-
-% Volume
-
-vol_cylinder = sub_radious^2 * cilynder_l * pi; % m^3
-vol_sphere = 4/3 * sub_radious^3 * pi; % m^3
-sub_volume = vol_cylinder + vol_sphere; % m^3
-
-istisnina = row * sub_volume; % t
-
-CB =  sub_volume / (sub_length * 2 * sub_radious * 2 * sub_radious); % Block coefficient
-
+radius_difference = radius_rb-radius_r;
+sub_length = length_c+2*radius_r;
 
 % Surface
 
-sur_cylinder = 2 * sub_radious * pi * cilynder_l;
-sur_sphere =  4 * sub_radious^2 * pi;
+surface_cylinder = 2*radius_r*pi*length_c;
+surface_sphere = 4*radius_r^2*pi;
+surface_total = surface_cylinder+surface_sphere;
 
-sub_surface = sur_cylinder + sur_sphere;
+% Volume
+
+volume_cylinder = radius_r^2*pi*length_c;
+volume_sphere = 4/3*radius_r^3*pi;
+volume_total = volume_cylinder+volume_sphere;
+istisnina = volume_total*row/1000;
+
+
+CB = volume_total/(sub_length*2*radius_r*2*radius_r);
+
+
+
 
 % Debljina stjenke
 
-Di = 2 * sub_radious * 1000;
+pressure_conversion = 100000;
 
-sub_stijenka = p_h_bar * Di / (20 * sigma * faktor_zavarivanja + p_h_bar);
+depth_pressure_pa = row*g*max_depth;
+depth_pressure_b = depth_pressure_pa/pressure_conversion;
 
-sub_masa_celika = sub_surface * sub_stijenka/1000 * roc/1000 * faktor_strukture;
+depth_pressure_b = depth_pressure_b;
+Di = radius_r*2*1000;
+
+thickness_hull = depth_pressure_b*Di/(20*sigma+depth_pressure_b);
+
+
+mass_steel = surface_total*thickness_hull/1000*roc/1000*1.5;
 
 
 end
--- Submarine/submarine_mechanical_generated.m
+++ Submarine/submarine_mechanical_generated.m
@@ -1,9 +1,14 @@
 
 
-row = 1025;
-roc = 7850;
-g = 9.81;
+%% Constants
 
+% Densities
+row = 1025; % kg/m^3 - Salt water
+roc = 7850; % kg/m^3 - Steel
+rop = 1300; % kg/m^3 - Plastics
+
+% Physical constants
+g = 9.81; % m/s^2
 
 max_depth = 50;
 passenger_count2 = 2;
@@ -14,19 +19,30 @@ speed_kn = 7;
 
 MCR = 0.85;
 
+sigma = 125;
+k_weld = 1;
+k_structure = 1.5;
+
 length_c = 11.05;   % m
 radius_r = 1.5;     % m
 radius_rb = 2.2;    % m
 
 power_density_turbine = 0.1; % t/kW
 
+
+%% Calculations
+
+
 radius_difference = radius_rb-radius_r;
 sub_length = length_c+2*radius_r;
+
+% Surface
 
 surface_cylinder = 2*radius_r*pi*length_c;
 surface_sphere = 4*radius_r^2*pi;
 surface_total = surface_cylinder+surface_sphere;
 
+% Volume
 
 volume_cylinder = radius_r^2*pi*length_c;
 volume_sphere = 4/3*radius_r^3*pi;
@@ -37,33 +53,26 @@ istisnina = volume_total*row/1000;
 CB = volume_total/(sub_length*2*radius_r*2*radius_r);
 
 
-pressure_conversion = 100000;
 
+
+% Debljina stjenke
+
+pressure_conversion = 100000;
 
 depth_pressure_pa = row*g*max_depth;
 depth_pressure_b = depth_pressure_pa/pressure_conversion;
 
-
-
-
-
-
-
-
 depth_pressure_b = depth_pressure_b;
 Di = radius_r*2*1000;
-sigma = 125;
-k_weld = 1;
-
 
 thickness_hull = depth_pressure_b*Di/(20*sigma+depth_pressure_b);
 
 
-mass_steel = surface_total*thickness_hull/1000*roc/1000*1.5;
+mass_steel = surface_total*thickness_hull/1000*roc/1000*k_structure;
 
+% Power
 
 kn2ms = 1852/3600;
-
 
 speed_kn = speed_kn;
 speed_ms = speed_kn*kn2ms;

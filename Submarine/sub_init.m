164c3c725cfc28c1cc3acd096fed04097fa017d1
Ante Orešković <ante.oreskovic@gideonbros.ai>
2019-05-10 02:41:08 +0200

    adhjoč<xKCLHVc< ykcBKJXCBJČKL

--- Submarine/sub_init.m
+++ Submarine/sub_init.m
@@ -1,10 +1,14 @@
 
 clear all
 
-sub.radius_dry = 1.5;
+sub.radius_dry = 1.50;
 sub.radius_wet = 1.7
 sub.length = 15
 sub.max_depth = 200
 
 sub.mass = sub_mass(sub.length, sub.radius_dry, sub.radius_wet, ...
-                    sub.max_depth, 0, 4)
+                    sub.max_depth, 1.2, 4);
+                
+                
+%sub.mass = sub_mass(sub.length, sub.radius_dry, sub.radius_wet, ...
+%                    sub.max_depth, 1.4, 0)
